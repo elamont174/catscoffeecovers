bd41c9cd905cf542f2036be6792b69d13ff04776
Emma Lamont <ejwl_92@hotmail.com>
2024-11-13 20:17:48 +0000

    Add sortable fields using the annotate method

--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,34 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from catscoffeecovers.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
 class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        bookreviews_count=Count('owner__bookreview', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'bookreviews_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
-
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        bookreviews_count=Count('owner__bookreview', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
