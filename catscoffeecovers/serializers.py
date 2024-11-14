fe2dc23de755fc77a3f41285021cc8d4219ade44
Emma Lamont <ejwl_92@hotmail.com>
2024-11-14 19:25:12 +0000

    ADd jsno web tokens

---
+++ catscoffeecovers/serializers.py
@@ -0,0 +1,12 @@
+from dj_rest_auth.serializers import UserDetailsSerializer
+from rest_framework import serializers
+
+
+class CurrentUserSerializer(UserDetailsSerializer):
+    profile_id = serializers.ReadOnlyField(source='profile.id')
+    profile_profile_pic = serializers.ReadOnlyField(source='profile.profile_pic.url')
+
+    class Meta(UserDetailsSerializer.Meta):
+        fields = UserDetailsSerializer.Meta.fields + (
+            'profile_id', 'profile_profile_pic'
+        )
