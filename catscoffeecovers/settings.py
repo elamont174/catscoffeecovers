645fb10f98f10555c265fc0129cd1eda5f87e2db
Emma Lamont <ejwl_92@hotmail.com>
2024-11-21 11:15:14 +0000

    Close [] in settings.py

--- catscoffeecovers/settings.py
+++ catscoffeecovers/settings.py
@@ -108,7 +108,7 @@ if "CLIENT_ORIGIN" in os.environ:
     CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^https://.*\.codeinstitute-ide\.net$"
+    r"^https://.*\.codeinstitute-ide\.net$"]
 
 
 CORS_ALLOW_CREDENTIALS = True
