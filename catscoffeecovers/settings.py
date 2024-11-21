7664ea5740bf2485ae343c9f99153dd827cd78b5
Emma Lamont <ejwl_92@hotmail.com>
2024-11-21 11:06:32 +0000

    Debug by updating CLIENT_ORIGIN

--- catscoffeecovers/settings.py
+++ catscoffeecovers/settings.py
@@ -104,13 +104,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-    ]
+if "CLIENT_ORIGIN" in os.environ:
+    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://3000-elamont174-catscoffeeco-c8byfjme9hi.ws.codeinstitute-ide\.net$"]
+
 
 CORS_ALLOW_CREDENTIALS = True
+
+
 
 ROOT_URLCONF = 'catscoffeecovers.urls'
 
