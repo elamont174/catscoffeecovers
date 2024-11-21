ce63ae7166ea1b3d0b0ff117980906b52f957c57
Emma Lamont <ejwl_92@hotmail.com>
2024-11-21 17:01:51 +0000

    Debug by changing order of MIDDLEWARE and updating requirements

--- catscoffeecovers/settings.py
+++ catscoffeecovers/settings.py
@@ -84,11 +84,11 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken',
     'dj_rest_auth',
-    'dj_rest_auth.registration',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'dj_rest_auth.registration',
     'corsheaders',
     'profiles',
     'book_reviews',
@@ -107,7 +107,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
+
 ]
 
 if "CLIENT_ORIGIN" in os.environ:
