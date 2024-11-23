7d7125a70f15b7ee10be0ab71df509261b6a7126
Emma Lamont <ejwl_92@hotmail.com>
2024-11-23 02:33:28 +0000

    Debug by changing profile_pic to image

--- catscoffeecovers/serializers.py
+++ catscoffeecovers/serializers.py
@@ -4,10 +4,10 @@ from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_profile_pic = serializers.ReadOnlyField(
-        source='profile.profile_pic.url')
+    profile_image = serializers.ReadOnlyField(
+        source='profile.image.url')
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_profile_pic'
+            'profile_id', 'profile_image'
         )
--- profiles/models.py
+++ profiles/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=False)
     image = models.ImageField(
-        upload_to='images/', default='../default_kejxo8')
+        upload_to='images/', default='../default_ifhb1q')
     bio = models.TextField(max_length=500, blank=True)
     location = models.TextField(max_length=255, blank=True)
     currently_reading = models.TextField(max_length=350, blank=True)
